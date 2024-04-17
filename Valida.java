b7f6b30b300f0130c52acff7cdc8341f9cfca857
SaraSilvaSoares <167377773+SaraSilvaSoares@users.noreply.github.com>
2024-04-17 20:39:31 -0300

    Add files via upload

---
+++ Valida.java
@@ -0,0 +1,11 @@
+public class Valida {
+ public static void main(String args[]) {
+    int idade = 17; 
+    if(idade >= 18){
+      System.out.println("Liberado");
+  } else {
+   System.out.println("Não Liberado");  
+
+  }
+ }
+}
